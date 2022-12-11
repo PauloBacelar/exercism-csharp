401a068144fd5f9997036776e2e135ecd1a85321
Paulo Bacelar <paulobacelara@gmail.com>
2022-12-11 09:16:05 -0300

    Initial commit

---
+++ Exercises/001HelloWorld.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercism.Exercises {
+    internal class _001HelloWorld {
+        public static void Executar() {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Exercism.Exercises;
+
+namespace CursoCSharp {
+    class Program {
+        static void Main(string[] args) {
+            var central = new CentralDeExercicios(new Dictionary<string, Action>() {
+                {"001 - Hello World", _001HelloWorld.Executar},
+            });
+
+            central.SelecionarEExecutar();
+        }
+    }
+}
