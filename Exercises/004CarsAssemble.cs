9b6de220cdc8a01f5557a9a8725c18ad58a8477b
Paulo Bacelar <paulobacelara@gmail.com>
2022-12-12 17:00:48 -0300

    Solved exercise 004 - Assembly Line

---
+++ Exercises/004CarsAssemble.cs
@@ -0,0 +1,33 @@
+﻿namespace Exercism.Exercises {
+    class AssemblyLine {
+        public static double SuccessRate(int speed) {
+            if (speed == 0) {
+                return 0;
+            } else if (speed >= 1 && speed <= 4) {
+                return 1;
+            } else if (speed >= 5 && speed <= 8) {
+                return 0.9;
+            } else if (speed == 9) {
+                return 0.8;
+            } else if (speed == 10) {
+                return 0.77;
+            }
+
+            return 0;
+        }
+
+        public static double ProductionRatePerHour(int speed) {
+            return speed * 221 * SuccessRate(speed);
+        }
+
+        public static int WorkingItemsPerMinute(int speed) {
+            return (int) ProductionRatePerHour(speed) / 60;
+        }
+
+        public static void Executar() {
+            Console.WriteLine(SuccessRate(10));
+            Console.WriteLine(ProductionRatePerHour(6));
+            Console.WriteLine(WorkingItemsPerMinute(6));
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -9,7 +9,8 @@ namespace CursoCSharp {
             var central = new CentralDeExercicios(new Dictionary<string, Action>() {
                 {"001 - Hello World", HelloWorld.Executar},
                 {"002 - Lucian's Luscious Lasagna", LusciousLasagna.Executar},
-                {"003 -  Annalyn's Infiltration ", QuestLogic.Executar},
+                {"003 -  Annalyn's Infiltration", QuestLogic.Executar},
+                {"004 -  Cars, Assmeble!", AssemblyLine.Executar},
             });
 
             central.SelecionarEExecutar();
